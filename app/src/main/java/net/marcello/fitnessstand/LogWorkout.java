40cfe419b619eb15eda3aa6a9df586971b35df08
Marcello Fabbri <marcellogfabbri@gmail.com>
2020-04-23 19:54:18 +0100

    add new activity (for logging new workouts)

---
+++ app/src/main/java/net/marcello/fitnessstand/LogWorkout.java
@@ -0,0 +1,16 @@
+package net.marcello.fitnessstand;
+
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LogWorkout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_workout);
+    }
+}
